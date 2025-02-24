9878906c0d27ef76fe214d7171f9e3894172863b
Ifechukwu Umeigbo <kennyumeigbo@yahoo.com>
2025-02-23 23:36:58 -0400

    Added and documented a copy of the cars dataset.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Documentation of the Built-in 'cars' Dataset
+#'
+#' The `cars` dataset is a built-in dataset in R that provides information about the speed of cars and the distances taken to stop.
+#'
+#'
+#' @format ## `who`
+#' A data frame with 50 observations and 2 variables:
+#' \describe{
+#'   \item{speed}{The speed of the car. Numeric value.}
+#'   \item{dist}{The stopping distance of the car. Numeric value.}
+#' }
+#' @source S. Ehrenstein's "Das Gesetz der Anstrengung" (1924).
